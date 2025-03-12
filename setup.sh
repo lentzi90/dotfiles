0fece1afc42fccd7132893fa66dbc125572b34b8
Lennart Jern <lennart.jern@est.tech>
2025-03-12 12:35:31 +0200

    The humble beginning

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+PERSONA="${1:-private}"
+DRY_RUN=${DRY_RUN:-false}
+if [[ $# -eq 1 ]]; then
+  if [[ "${1}" == "--dry-run" ]]; then
+    DRY_RUN=true
+    PERSONA=private
+  fi
+fi
+if [[ "${2}" == "--dry-run" ]]; then
+  DRY_RUN=true
+fi
+
+# Create symbolic links for every file in the PERSONA directory to the home directory
+for file in $(find "${PERSONA}" -type f); do
+  if [[ "${DRY_RUN}" == "true" ]]; then
+    echo "ln --symbolic --force $(pwd)/${file} ${HOME}/$(basename ${file})"
+  else
+    ln --symbolic --force "$(pwd)/${file}" "${HOME}/"
+  fi
+done
